ad7a8e0210b8e00d8c2d6aacd328512cf74b8083
KajalBansal25 <kajal.bansal@daffodilsw.com>
2022-07-15 15:29:29 +0530

    searching functionality in flutter

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: SearchPage(),
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  SearchPageState createState() => SearchPageState();
+}
+
+class SearchPageState extends State<SearchPage> {
+  String? result;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Search')),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text(result ?? ' ', style: const TextStyle(fontSize: 18)),
+            ElevatedButton(
+              onPressed: () async {
+                var result = await showSearch<String>(
+                  context: context,
+                  delegate: CustomDelegate(),
+                );
+                setState(() => result = result);
+              },
+              child: const Text('Search'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomDelegate extends SearchDelegate<String> {
+  List<String> data = nouns.take(100).toList();
+
+  @override
+  List<Widget> buildActions(BuildContext context) => [IconButton(icon: const Icon(Icons.clear), onPressed: () => query = '')];
+
+  @override
+  Widget buildLeading(BuildContext context) => IconButton(icon: const Icon(Icons.chevron_left), onPressed: () => close(context, ''));
+
+  @override
+  Widget buildResults(BuildContext context) => Container();
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    var listToShow;
+    if (query.isNotEmpty)
+      listToShow = data.where((e) => e.contains(query) && e.startsWith(query)).toList();
+    else
+      listToShow = data;
+
+    return ListView.builder(
+      itemCount: listToShow.length,
+      itemBuilder: (_, i) {
+        var noun = listToShow[i];
+        return ListTile(
+          title: Text(noun),
+          onTap: () => close(context, noun),
+        );
+      },
+    );
+  }
+}
